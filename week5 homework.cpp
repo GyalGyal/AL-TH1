41ee79e237f788fe607d39cb2c6f606a47f5af0a
GyalGyal <siyul13gm@gmail.com>
2025-07-12 11:41:54 +0900

    Add w4 homework

---
+++ week5 homework.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int m[1000];
+	int n;
+	int h;
+	int count = 0;
+	int sum = 0;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &h);
+		m[i] = h;
+
+		if (m[n - (i+1)] <= m[n - (i+2)])
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
+}
