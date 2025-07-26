b392b9525ff7890e0d063bf5fc5ec4a1c46e80bb
GyalGyal <siyul13gm@gmail.com>
2025-07-26 13:00:25 +0900

    Add w7

---
+++ AL-Week7.cpp
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+/*
+int main()
+{
+	char str[1001];
+	scanf("%[^\n]",str);
+	for (int i = 0; str[i]; i++)
+	{
+		if ('A' <= str[i] && str[i] <= 'Z')
+		{
+			str[i] = str[i] - 'A' + 'a';
+		}
+	}
+	printf("%s", str);
+}
+
+
+int main()
+{
+	char str[1001];
+	int count[26] = {0. };
+	scanf("%s", str);
+	for (int i = 0; str[i]; i++)
+	{
+		if ('a' <= str[i] && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+			count[str[i] - 'A']++;
+		}
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		printf("%d ", count[i]);
+	}
+}
+
+
+int main()
+{
+	char str[1001];
+	int n = 0;
+	scanf("%d", &n);
+	scanf("%s", str);
+	for (int i = 0; str[i]; i++)
+	{
+		str[i] = str[i] + n;
+		if (str[i] > 'Z')
+		{
+			str[i] = str[i] - 26;
+			
+		}
+		printf("%c", str[i]);
+	}
+
+}
+
+
+int main()
+{
+	char str[1001];
+	int count[26];
+	for (int i = 0; i < 26; i++)
+	{
+		count[i] = -1;
+	}
+	scanf("%s", str);
+	for (int i = 0; str[i]; i++)
+	{
+		if (count[str[i] - 'a'] == -1)
+		{
+			count[str[i] - 'a'] = i;
+		}
+		
+	}
+
+	for (int i = 0; i < 26; i++) {
+		printf("%d ", count[i]);
+	}
+}
+*/
+
+void max(int a[10000], int b[])
+{
+	for(int i = 0; a[i]; )
+}
+
+
+
+int main()
+{
+	int a[10000];
+	int b[10000];
+	scanf("%d %d", &a, &b);
+	
+
+}
+
+
+
