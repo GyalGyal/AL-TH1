3bce2ea31f16bef2e65a57d780727bd387e35a9a
GyalGyal <siyul13gm@gmail.com>
2025-08-08 21:37:09 +0900

    Add w8 homework

---
+++ week8 homework.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s;
+	string n;
+	cin >> s;
+	cin >> n;
+	
+	s.c_str();
+	
+	for (int i = 0; i <= s.size(); i++) {
+
+		string combine = s + n;
+
+		cout << combine.substr(i);
+		printf("\n");
+	}
+
+
+
+}
