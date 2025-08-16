9ae6a82604182669d67038782a756d84511e561b
GyalGyal <siyul13gm@gmail.com>
2025-08-16 12:27:50 +0900

    Add w9 homework

---
+++ week9 homework.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string>
+#include<iostream>
+#include <string.h>
+using namespace std;
+
+
+int main()
+{
+	string a;
+	string b;
+	int count = 0;
+
+	cin >> a >> b;
+
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (a.substr(i, b.size()) == b)
+		{
+			count ++;
+			i += b.size();
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
